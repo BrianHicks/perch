57cdb7cd58e2a6d81f1384d8fe811df8a011e9bd
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-10-07 19:03:07 -0500

    refactor to group by class

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,24 +7,25 @@ from perch.config import Settings
 def settings():
     return Settings(a=1)
 
-def test_get_dot(settings):
-    'getting works with __getattr__'
-    assert settings.a == 1
+class TestSettings(object):
+    def test_get_dot(self, settings):
+        'getting works with __getattr__'
+        assert settings.a == 1
 
-def test_get_dict(settings):
-    'getting works with __getitem__'
-    assert settings['a'] == 1
+    def test_get_dict(self, settings):
+        'getting works with __getitem__'
+        assert settings['a'] == 1
 
-def test_set_dot(settings):
-    'setting does not work with __setattr__'
-    with pytest.raises(TypeError):
-        settings.a = 2
+    def test_set_dot(self, settings):
+        'setting does not work with __setattr__'
+        with pytest.raises(TypeError):
+            settings.a = 2
 
-    assert settings.a == 1
+        assert settings.a == 1
 
-def test_set_dict(settings):
-    'setting does not work with __setitem__'
-    with pytest.raises(TypeError):
-        settings['a'] = 2
+    def test_set_dict(self, settings):
+        'setting does not work with __setitem__'
+        with pytest.raises(TypeError):
+            settings['a'] = 2
 
-    assert settings.a == 1
+        assert settings.a == 1
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,13 +12,14 @@ def filledregistry(registry):
     registry.register('test')(1)
     return registry
 
-def test_registers_name_value(registry):
-    registry.register('test')(1)
+class TestClassRegistery(object):
+    def test_registers_name_value(self, registry):
+        registry.register('test')(1)
 
-    assert registry == {'test': 1}
+        assert registry == {'test': 1}
 
-def test_access_name(filledregistry):
-    assert filledregistry['test'] == 1
+    def test_access_name(self, filledregistry):
+        assert filledregistry['test'] == 1
 
-def test_get_name(filledregistry):
-    assert filledregistry.get('test') == 1
+    def test_get_name(self, filledregistry):
+        assert filledregistry.get('test') == 1
